3bb5212e9b5a0a55f1b2cfdf6bd391ccfc62726c
jefeez <jefersonquez@gmail.com>
2024-06-16 13:07:25 -0300

    modify audio

--- audio.py
+++ audio.py
@@ -1,7 +1,13 @@
 import pyaudio
 
+# Inicializa o PyAudio
 p = pyaudio.PyAudio()
 
+# Lista os dispositivos de áudio disponíveis
+print("Lista de dispositivos de áudio:")
 for i in range(p.get_device_count()):
     info = p.get_device_info_by_index(i)
-    print(info)
+    print(f"Dispositivo {i}: {info['name']} - Entradas: {info['maxInputChannels']}, Saídas: {info['maxOutputChannels']}")
+
+# Fecha o PyAudio
+p.terminate()
